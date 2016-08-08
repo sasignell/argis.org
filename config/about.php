c44988ce68de6ad77faa03e4bcaebd31448063cf
Steve Signell <steve@frontierspatial.com>
2016-08-07 21:57:48 -0400

    fixed broken links

--- config/about.php
+++ config/about.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" type="text/css" href="http://www.aprgis.org:8080/geoserver/www/aprgis/theme/app/geoexplorer.css" />
+<link rel="stylesheet" type="text/css" href="http://www.argis.org:8080/geoserver/www/aprgis/theme/app/geoexplorer.css" />
 <p><b><font size="4">About ARGIS</font></b></p>
 </br><b> ATTENTION: THIS SITE IS NO LONGER BEING ACTIVELY MAINTAINED OR MANAGED: NO DATA UPDATES ARE PLANNED UNTIL FUNDING IS PROCURED.</b></br></br><p>The Adirondack Regional Geographic Information System (ARGIS) is a regional spatial data portal designed to serve authoritative regional data.</br> Created by Steve Signell (<a href="http://frontierspatial.com" target="_blank">Frontier Spatial, LLC<a/>) and Bryan McBride (<a href="http://geoserving.net" target="_blank">geoserving.net</a>), ARGIS is funded by a grant from the New York State Department of Environmental Conservation and contributions from data providers. ARGIS is currently serving data from:</p>
 	<p><ul class="ulCls">
@@ -31,6 +31,6 @@
 	<br/>
 	<p><b>ARGIS Architecture:</b></p>
 	<p>ARGIS is built on a LINUX server using free, open-source software:</p>
-	<div class="center"><img style="width: 600px; height: 186px;" src="http://www.aprgis.org/argis/img/technology.png">
+	<div class="center"><img style="width: 600px; height: 186px;" src="http://www.argis.org/argis/img/technology.png">
 	</div>
 	</div>
--- config/help.php
+++ config/help.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" type="text/css" href="http://www.aprgis.org:8080/geoserver/www/aprgis/theme/app/geoexplorer.css" />
+<link rel="stylesheet" type="text/css" href="http://www.argis.org:8080/geoserver/www/aprgis/theme/app/geoexplorer.css" />
 <p><font size="4"><b>ARGIS Help & Tutorials</b></font></p>
 </br>
 <p><font size="3"><a href="#toolbar">Using the Toolbar</a></br>
@@ -7,31 +7,31 @@
 <a href="#download">Downloads and Web Map Services</a></br>
 
 <a name="toolbar"><p><font size="3"><b>Toolbar</b></font></p>
-<img src="http://www.aprgis.org/argis/img/geosilk/handpan.png"/>
+<img src="http://www.argis.org/argis/img/geosilk/handpan.png"/>
 <b>   Pan:</b> click-hold and move mouse to pan the map.</br>
 
-<img src="http://www.aprgis.org/argis/img/silk/magnifier_zoom_in.png"/>
+<img src="http://www.argis.org/argis/img/silk/magnifier_zoom_in.png"/>
 <b>   Zoom in:</b> single click on the map will perfom fixed zoom in, or click-hold and draw a polygon to zoom to a specific area.  </br>
 
-<img src="http://www.aprgis.org/argis/img/silk/magnifier_zoom_out.png"/>
+<img src="http://www.argis.org/argis/img/silk/magnifier_zoom_out.png"/>
 <b>  Zoom out:</b> single click on the map will perform fixed zoom out.</br>
 
-<img src="http://www.aprgis.org/argis/img/silk/arrow_left.png"/> 
+<img src="http://www.argis.org/argis/img/silk/arrow_left.png"/> 
 <b>   Previous extent:</b> toggle back to last map extent.</br>
 
-<img src="http://www.aprgis.org/argis/img/silk/arrow_right.png"/>
+<img src="http://www.argis.org/argis/img/silk/arrow_right.png"/>
 <b>   Next extent:</b> toggle forward to next map extent.</br>
 
-<img src="http://www.aprgis.org/argis/img/silk/arrow_out.png"/>
+<img src="http://www.argis.org/argis/img/silk/arrow_out.png"/>
    <b>   Full extent:</b> zoom to full extent of the active layers.</br>
 
-<img src="http://www.aprgis.org/argis/img/silk/information.png"/>
+<img src="http://www.argis.org/argis/img/silk/information.png"/>
    <b>   Information:</b> click on features on the map to get more information, reports and/or links to other datasets or websites.</br>
 
-<img src="http://www.aprgis.org/argis/img/geosilk/ruler.png"/>
+<img src="http://www.argis.org/argis/img/geosilk/ruler.png"/>
    <b>   Ruler:</b> click on the map to calculate line distances or polygon areas.</br>
 
-<img src="http://www.aprgis.org/argis/img/silk/find.png"/>
+<img src="http://www.argis.org/argis/img/silk/find.png"/>
    <b>   Subset and Download:</b> open Query Dialogue to filter and/or download data by geographic extent, view attribute tables.
 </br>
 </br>
@@ -48,7 +48,7 @@ There are several ways to bring data from ARGIS to your local computer:</br>
 2. If you only need data for a portion of the layer extent, click on our <img src="img/subset.png"/> tool to zoom in and extract the data you want.</br>
 </br>
 3. If would like to view the layers in your own mapping application (e.g. ArcGIS, Google Earth or uDig), but do not need/want to download the raw data, you can use the following URL to add the <span style="color:red"><b>Adirondack Web Map Service</b></span> to your application:</br></br>
-<b><u><span style="color:blue"> http://aprgis.org:8080/geoserver/ows?service=wms&version=1.1.1&request=GetCapabilities&</span></u></b>
+<b><u><span style="color:blue"> http://argis.org:8080/geoserver/ows?service=wms&version=1.1.1&request=GetCapabilities&</span></u></b>
 </br>
 </br>
 </br>
